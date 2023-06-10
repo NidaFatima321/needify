72f6beb8729683a21a5dfe62a999a84babdf2ee5
Anoosha Batool <anooshabatool2001@gmail.com>
2023-06-10 20:57:56 +0500

    drawer added

--- lib/Views/dashboard_needify.dart
+++ lib/Views/dashboard_needify.dart
@@ -1,10 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:needify/Views/category_widget.dart';
 import 'package:needify/Views/laptops.dart';
 import 'package:needify/Views/notes.dart';
 import 'package:needify/Views/sold_items.dart';
 import 'package:needify/main.dart';
+
+import '../reusable_widgets/round_image.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -14,6 +17,9 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  final currentUser = FirebaseAuth.instance;
+  var currentPage = DrawerSelections.Home;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +31,67 @@ class _DashboardState extends State<Dashboard> {
                 width: 300,
                 child: Text("Faisal Apartments Johar Karachi"))
           ],
+        ),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("Users")
+                        .where("Email", isEqualTo: currentUser.currentUser!.email)
+                        .snapshots(),
+                    builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasData){
+                        return ListView.builder(
+                            itemCount: snapshot.data!.docs.length,
+                            shrinkWrap: true,
+                            itemBuilder: (context, index) {
+                              var data = snapshot.data!.docs[index];
+                              return Container(
+                                color: Colors.pinkAccent,
+                                width: double.infinity,
+                                height: 200,
+                                padding: EdgeInsets.only(top: 20,),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    AppRoundImage.url(
+                                        data["User image"].toString(),
+                                        height: 80,
+                                        width: 80
+                                    ),
+                                    //Image.network(data["User image"].toString()),
+                                    Text(
+                                      data["Name"],
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    Text(
+                                      data["Email"],
+                                      style: TextStyle(
+                                        color: Colors.grey[200],
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+
+                            }
+                        );
+                      }else{
+                        return CircularProgressIndicator();
+                      }
+                    }),
+                MyDrawerList()
+              ],
+            ),
+          ),
         ),
       ),
       body: Padding(
@@ -83,4 +150,69 @@ class _DashboardState extends State<Dashboard> {
 
     );
   }
+  Widget MyDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(top: 70,),
+      child: Column(
+        children: [
+          menuItem(1, "Dashboard", Icons.dashboard_outlined,
+              currentPage == DrawerSelections.Home? true : false),
+          menuItem(1, "My Purchases", Icons.shopping_bag_outlined,
+              currentPage == DrawerSelections.my_purchases? true : false),
+          menuItem(1, "Sold Items", Icons.sell_outlined,
+              currentPage == DrawerSelections.sold_items? true : false),
+        ],
+      ),
+    );
+  }
+
+  Widget menuItem(int id, String title, IconData icon, bool selected) {
+    return Material(
+      color: selected ? Colors.grey[300] : Colors.transparent,
+      child: InkWell(
+        onTap: (){
+          Navigator.pop(context);
+          setState(() {
+            if (id == 1) {
+              currentPage == DrawerSelections.Home;
+            } else if (id == 2) {
+              currentPage == DrawerSelections.my_purchases;
+            } else if (id == 3) {
+              currentPage == DrawerSelections.sold_items;
+            }
+          });
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15),
+          child: Row(
+            children: [
+              Expanded(
+                  child: Icon(
+                    icon,
+                    size: 20,
+                    color: Colors.black,
+                  )
+              ),
+              Expanded(
+                  flex: 3,
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+enum DrawerSelections {
+  Home,
+  my_purchases,
+  sold_items
 }
