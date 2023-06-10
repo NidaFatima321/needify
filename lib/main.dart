ae562bf35f91c8d4a42d2ed0b51406c78fd45e94
Aiman-Waqar <sarwatwaqr@gmail.com>
2023-06-10 19:02:07 +0500

    added add post and my purchases file

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:needify/Views/dashboard_needify.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'Views/add_post.dart';
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MaterialApp(
-    home: Dashboard(),
+    home: AddPost(),
   ));
 }
 String login="shaikhnaila488@gmail.com";
