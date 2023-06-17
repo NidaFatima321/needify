8ac59f0b4d2f1eb5083b3708f3dfb7841b390a4c
Nida Fatima <fatima4302887@cloud.neduet.edu.pk>
2023-06-17 18:08:45 +0500

    jazzcash solds update

--- lib/Views/jazzcash_screen.dart
+++ lib/Views/jazzcash_screen.dart
@@ -5,14 +5,20 @@ import 'package:needify/main.dart';
 
 import 'checkoutScreen.dart';
 
-
 class JazzCashScreen extends StatefulWidget {
   final String productPrice;
   final String productName;
   final CollectionReference collectionReference;
   final DocumentSnapshot postsData;
   final DocumentSnapshot docssnap;
-  const JazzCashScreen({Key? key,required this.productPrice,required this.productName, required this.collectionReference, required this.postsData, required this.docssnap}) : super(key: key);
+  const JazzCashScreen(
+      {Key? key,
+      required this.productPrice,
+      required this.productName,
+      required this.collectionReference,
+      required this.postsData,
+      required this.docssnap})
+      : super(key: key);
 
   @override
   State<JazzCashScreen> createState() => _JazzCashScreenState();
@@ -20,13 +26,13 @@ class JazzCashScreen extends StatefulWidget {
 
 class _JazzCashScreenState extends State<JazzCashScreen> {
   String paymentStatus = "pending";
- // ProductModel productModel = ProductModel("Product 1", "100");
-  String integritySalt= "yutez0uw01";
-  String merchantID= "MC57762";
+  // ProductModel productModel = ProductModel("Product 1", "100");
+  String integritySalt = "yutez0uw01";
+  String merchantID = "MC57762";
   String merchantPassword = "4zc318v04w";
-  String transactionUrl= "https://sandbox.jazzcash.com.pk/ApplicationAPI/API/Payment/DoTransaction";
+  String transactionUrl =
+      "https://sandbox.jazzcash.com.pk/ApplicationAPI/API/Payment/DoTransaction";
   // String transactionUrl= "jazzcashtest://payment-response";
-
 
   @override
   Widget build(BuildContext context) {
@@ -37,16 +43,16 @@ class _JazzCashScreenState extends State<JazzCashScreen> {
           children: [
             Text("Product Name : ${widget.productName}"),
             Text("Product Price : ${widget.productPrice}"),
-            ElevatedButton(onPressed: () {
-              _payViaJazzCash( context);
-
-            }, child: const Text("Confirm to Purchase Now !"))
+            ElevatedButton(
+                onPressed: () {
+                  _payViaJazzCash(context);
+                },
+                child: const Text("Confirm to Purchase Now !"))
           ],
         ),
       ),
     );
   }
-
 
   Future _payViaJazzCash(BuildContext c) async {
     //print("clicked on Product ${element.productName}");
@@ -57,56 +63,79 @@ class _JazzCashScreenState extends State<JazzCashScreen> {
         merchantPassword: merchantPassword,
         integritySalt: integritySalt,
         isSandbox: true,
-
       );
 
       DateTime date = DateTime.now();
 
-      JazzCashPaymentDataModelV1 paymentDataModelV1 = JazzCashPaymentDataModelV1(
+      JazzCashPaymentDataModelV1 paymentDataModelV1 =
+          JazzCashPaymentDataModelV1(
         ppAmount: '${widget.productPrice}',
-        ppBillReference:'refbill${date.year}${date.month}${date.day}${date.hour}${date.millisecond}',
-        ppDescription: 'Product details  ${widget.productName} - ${widget.productPrice}',
+        ppBillReference:
+            'refbill${date.year}${date.month}${date.day}${date.hour}${date.millisecond}',
+        ppDescription:
+            'Product details  ${widget.productName} - ${widget.productPrice}',
         ppMerchantID: merchantID,
-        ppPassword:  merchantPassword,
+        ppPassword: merchantPassword,
         ppReturnURL: transactionUrl,
       );
 
-      jazzCashFlutter.startPayment(paymentDataModelV1: paymentDataModelV1, context: context).then((_response) {
+      jazzCashFlutter
+          .startPayment(
+              paymentDataModelV1: paymentDataModelV1, context: context)
+          .then((_response) {
         print("response from jazzcash $_response");
-              widget.collectionReference
-                  .doc(widget.postsData.id)
-                  .update({"Status": "Sold"});
+        widget.collectionReference
+            .doc(widget.postsData.id)
+            .update({"Status": "Sold"});
 
-              List<dynamic> news = [];
-              for (int i = 0;
-              i < maindata!["Purchased"].length;
-              i++) {
-                news.add(maindata!["Purchased"][i]);
-              }
-              print(maindata!["Name"]);
-              news.add(widget.postsData.id);
-              print(news);
-              FirebaseFirestore.instance
-                  .collection("Users")
-                  .doc(login)
-                  .update({"Purchased": news});
-              print(maindata!.id);
-              String logid =
-                  widget.collectionReference.parent!.id;
-              List<dynamic> soldItems = [];
-              for (int i = 0;
-              i < widget.docssnap["Sold"].length;
-              i++) {
-                soldItems.add(widget.docssnap["Sold"][i]);
-              }
-              soldItems.add(widget.postsData.id);
-              FirebaseFirestore.instance
-                  .collection("Users")
-                  .doc(logid)
-                  .update({"Sold": soldItems});
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => Checkout(),));
+        List<dynamic> news = [];
+        for (int i = 0; i < maindata!["Purchased"].length; i++) {
+          news.add(maindata!["Purchased"][i]);
+        }
+        print(maindata!["Name"]);
+        news.add(widget.postsData.id);
+        print(news);
+        FirebaseFirestore.instance
+            .collection("Users")
+            .doc(login)
+            .update({"Purchased": news});
+        print(maindata!.id);
+        String logid = widget.collectionReference.parent!.id;
+        // List<dynamic> soldItems = [];
+        // for (int i = 0;
+        // i < widget.docssnap["Sold"].length;
+        // i++) {
+        //   soldItems.add(widget.docssnap["Sold"][i]);
+        // }
+        // soldItems.add(widget.postsData.id);
+        // FirebaseFirestore.instance
+        //     .collection("Users")
+        //     .doc(logid)
+        //     .update({"Sold": soldItems});
+        Map<String, dynamic> maps = {};
+        List<dynamic> items = [];
+        for (int i = 0; i < widget.docssnap['Solds']['Items'].length; i++) {
+          items.add(widget.docssnap['Solds']['Items'][i]);
+        }
+        items.add(widget.postsData.id);
+        List<dynamic> Timestamps = [];
+        for (int i = 0;
+            i < widget.docssnap['Solds']['Timestamps'].length;
+            i++) {
+          items.add(widget.docssnap['Solds']['Timestamps'][i]);
+        }
+        Timestamps.add(DateTime.now());
+        maps = {"Items": items, "Timestamps": Timestamps};
+        FirebaseFirestore.instance
+            .collection("Users")
+            .doc(logid)
+            .update({"Solds": maps});
+        print(items);
+        print(Timestamps);
 
-
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) => Checkout(),
+        ));
 
         setState(() {});
       });
@@ -118,7 +147,6 @@ class _JazzCashScreenState extends State<JazzCashScreen> {
       return false;
     }
   }
-
 }
 
 // class ProductModel {
@@ -127,6 +155,3 @@ class _JazzCashScreenState extends State<JazzCashScreen> {
 //
 //   ProductModel(this.productName, this.productPrice);
 // }
-
-
-
